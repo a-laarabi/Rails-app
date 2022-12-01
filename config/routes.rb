a691900958adb1ddc29ee03b27f91d10f45c05f6
Anasse LAARABI <contact.laarabi@gmail.com>
2022-12-01 01:16:15 +0100

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  root 'root#index'
+  namespace :api do
+    namespace :v1 do
+      resources :messages, only: [:index]
+    end
+  end
+
+  root "root#index"
+  
 end
