c50cdd5bf13a88fd3744d0263538a25d72aa96f6
Anasse LAARABI <contact.laarabi@gmail.com>
2022-12-01 01:13:56 +0100

    Create migration && Update schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Message.create!(greeting: "This is greeting 1")
+Message.create!(greeting: "This is greeting 2")
+Message.create!(greeting: "This is greeting 3")
+Message.create!(greeting: "This is greeting 4")
+Message.create!(greeting: "This is greeting 5")
